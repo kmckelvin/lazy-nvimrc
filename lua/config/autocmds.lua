b5d37c6e52af13d38655de9f1d91ec1abe0a3d9c
Kevin McKelvin <kmckelvin@gmail.com>
2024-06-11 07:39:32 +0100

    Set tmux window title from nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+if vim.fn.exists("$TMUX") then
+  vim.api.nvim_create_autocmd(
+    { "VimEnter", "VimResume", "BufReadPost", "FileReadPost", "BufEnter" },
+    { command = 'call system("tmux rename-window vi:" . fnamemodify(getcwd(), ":t"))' }
+  )
+
+  vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, { command = 'call system("tmux setw automatic-rename")' })
+end
