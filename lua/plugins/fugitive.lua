d28618185a4f07b2112d7fbcce1cd24159b0ef63
Kevin McKelvin <kmckelvin@gmail.com>
2024-06-11 06:57:49 +0100

    Install vim-rhubarb for GitHub integration with Fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,11 @@
 return {
-  "tpope/vim-fugitive",
-  init = function()
-    vim.opt.grepprg = "rg --vimgrep"
-  end,
+  {
+    "tpope/vim-fugitive",
+    init = function()
+      vim.opt.grepprg = "rg --vimgrep"
+    end,
+  },
+  {
+    "tpope/vim-rhubarb",
+  },
 }
