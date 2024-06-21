72ff3a0c9c734927ce25722d9198055dc2f25ed9
Kevin McKelvin <kmckelvin@gmail.com>
2024-06-21 13:07:41 +0100

    gitsigns: Remove current line formatter opts
    The config was being warned as invalid

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,8 +10,5 @@ return {
       virt_text_priority = 100,
     },
     current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-    current_line_blame_formatter_opts = {
-      relative_time = false,
-    },
   },
 }
