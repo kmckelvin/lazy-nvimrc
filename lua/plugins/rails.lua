52dc92687d09db2cf407747e567697dc3c6ccee7
Kevin McKelvin <kmckelvin@gmail.com>
2024-06-21 13:20:52 +0100

    Configure Ruby for treesitter and endwise
    Use treesitter-endwise instead of the original tpope/vim-endwise

--- lua/plugins/rails.lua
+++ lua/plugins/rails.lua
@@ -1,3 +1,12 @@
 return {
-  "tpope/vim-rails",
+  { "tpope/vim-rails" },
+  { "RRethy/nvim-treesitter-endwise" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      endwise = {
+        enable = true,
+      },
+    },
+  },
 }
