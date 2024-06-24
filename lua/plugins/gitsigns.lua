f29a588e2cce292bfbc7d776cee152b909a69425
Kevin McKelvin <kmckelvin@gmail.com>
2024-06-24 16:57:18 +0100

    gitsigns: Tweak formatting and switch to eol format

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,11 +4,11 @@ return {
     current_line_blame = true,
     current_line_blame_opts = {
       virt_text = true,
-      virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
       delay = 200,
       ignore_whitespace = false,
       virt_text_priority = 100,
     },
-    current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+    current_line_blame_formatter = "     - <author>, <author_time:%Y-%m-%d> - <summary>",
   },
 }
