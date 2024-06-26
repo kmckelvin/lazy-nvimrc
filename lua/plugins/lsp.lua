6481aa81c9c4417695c77cdf3eb0dfb45dd2ec95
Kevin McKelvin <kmckelvin@gmail.com>
2024-06-26 18:49:55 +0100

    LSP: Add border to diagnostic window

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,11 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    diagnostics = {
+      float = {
+        border = "rounded",
+      },
+    },
     servers = {
       tsserver = {},
       standardrb = {},
