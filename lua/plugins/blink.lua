fcddde3005fbc95a377a77698a7546ca0d436004
Kevin McKelvin <kmckelvin@gmail.com>
2024-12-14 20:29:17 +0000

    Update LazyVim and reconfigure plugins for LazyVim 14

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,15 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      menu = {
+        border = "rounded",
+      },
+      documentation = {
+        window = {
+          border = "rounded",
+        },
+      },
+    },
+  },
+}
