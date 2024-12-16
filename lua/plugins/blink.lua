c2a69d2b6d47af34fb9554bf6968d324f94f6fac
Kevin McKelvin <kmckelvin@gmail.com>
2024-12-16 15:01:10 +0000

    Disable blink.cmp in markdown files

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,9 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    enabled = function()
+      return vim.bo.filetype ~= "markdown"
+    end,
     completion = {
       menu = {
         border = "rounded",
