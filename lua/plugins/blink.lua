6bcb12d63a95bb7ed1693f70f94e2b530f5b8ba2
Kevin McKelvin <kmckelvin@gmail.com>
2024-12-27 21:50:41 +0000

    Disable blink.cmp in prompts

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,7 +2,15 @@ return {
   "saghen/blink.cmp",
   opts = {
     enabled = function()
-      return vim.bo.filetype ~= "markdown"
+      if vim.bo.buftype == "prompt" then
+        return false
+      end
+
+      if vim.bo.filetype == "markdown" then
+        return false
+      end
+
+      return true
     end,
     completion = {
       menu = {
