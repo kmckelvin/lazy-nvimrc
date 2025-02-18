8fea2c533bf4a98277326a64da24f34b9950c035
Kevin McKelvin <kmckelvin@gmail.com>
2025-02-18 17:55:26 +0000

    Remove reference to telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- local telescope = require("telescope.builtin")
 local fzf = require("fzf-lua")
 vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>", { silent = true, desc = "Save the current buffer" })
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git status" })
