c7dc4ad129db5dba8878a5fd0b54aac16118a19c
Kevin McKelvin <kmckelvin@gmail.com>
2025-07-23 15:07:49 +0100

    Set vertical scroll speed in ghostty

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,7 @@ require("config.options.ruby")
 vim.opt.scrolloff = 5
 vim.opt.title = true
 vim.g.snacks_animate = false
+
+if vim.fn.getenv("TERM_PROGRAM") == "ghostty" then
+  vim.opt.mousescroll = "ver:1"
+end
