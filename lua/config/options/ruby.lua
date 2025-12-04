ed5986a6e25832b7cc2ad1761c2337fe5850ce58
Kevin McKelvin <kmckelvin@gmail.com>
2025-12-04 09:36:22 +0000

    Use rubocop if the codebase contains a .rubocop.yml file

--- lua/config/options/ruby.lua
+++ lua/config/options/ruby.lua
@@ -1,2 +1,22 @@
 -- vim.g.lazyvim_ruby_lsp = "solargraph"
-vim.g.lazyvim_ruby_formatter = "standardrb"
+
+-- Conditionally use rubocop if .rubocop.yml exists, otherwise use standardrb
+local function set_ruby_formatter()
+  local root_dir = vim.fn.getcwd()
+  local rubocop_config = root_dir .. "/.rubocop.yml"
+
+  if vim.fn.filereadable(rubocop_config) == 1 then
+    vim.g.lazyvim_ruby_formatter = "rubocop"
+  else
+    vim.g.lazyvim_ruby_formatter = "standardrb"
+  end
+end
+
+-- Set formatter on initial load
+set_ruby_formatter()
+
+-- Re-check formatter every time a Ruby file is opened
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  callback = set_ruby_formatter,
+})
